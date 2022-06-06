d522a97e6a08245688e22aded8f220d215cd34e9
kimsh <kim11221122aa@gmail.com>
2022-06-07 04:45:18 +0900

    #01

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject SelectedCard = null;
+    public GameObject UnSelectedCard = null;
+    public List<GameObject> Cards;
+    private int index;
+
+    void Update()
+    {
+        ViewUpdate();
+        SelectCard();
+    }
+
+    private void SelectCard()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+            if (hit.collider != null)
+            {
+                SelectedCard = hit.collider.gameObject;
+
+                if (SelectedCard != UnSelectedCard)
+                {
+                    UnSelectedCard = SelectedCard;
+                    for (int i = 0; i < Cards.Count; i++)
+                    {
+                        if (Cards[i] == SelectedCard)
+                        {
+                            StartCoroutine(Up(Cards[i].transform, 1f));
+                        }
+                        else
+                        {
+                            Down(Cards[i].transform);
+                        }
+                    }
+                }
+                else
+                {
+                    UnSelectedCard = null;
+                    Down(SelectedCard.transform);
+                }
+            }
+        }
+    }
+
+    private void ViewUpdate()
+    {
+        var cardsize = Cards[0].transform.localScale.x / 4;
+        var size = cardsize * (Cards.Count + 1) / 2;
+        for (int i = 0; i < Cards.Count; i++)
+        {
+            var sp = Cards[i].GetComponent<SpriteRenderer>();
+            sp.sortingOrder = i;
+            Cards[i].transform.localPosition = new Vector3(-size + ((i + 1) * cardsize), Cards[i].transform.localPosition.y, -i);
+        }
+    }
+
+    public IEnumerator Up(Transform obj, float distance)
+    {
+        while (obj.localPosition.y < distance)
+        {
+            yield return new WaitForSeconds(0.01f);
+            obj.localPosition = Vector3.MoveTowards(obj.localPosition, obj.localPosition + new Vector3(0, distance, 0), 0.15f);
+        }
+        obj.localPosition = new Vector3(obj.localPosition.x, distance, obj.localPosition.z);
+    }
+    public void Down(Transform obj)
+    {
+        obj.localPosition = new Vector3(obj.localPosition.x, 0, obj.localPosition.z);
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Interview2022_2
+
+**Basic Tasks**
+
+1. Add card selecting and deselecting feature.
+- On select, move the card up for certain time.
+- On deselect, move the card down instantly.
+![cardselect](CardSelect.gif)
+
+2. Add card dragging feature.
+- On drag start, move the card up instantly.
+- On dragging, fix the y position of the card.
+- When the dragging card gets closer to another card, move the position of the other card toward the dragging card.
+- On drag end, move the card down instantly.
+![carddrag](CardDrag.gif)
+
+
+**Additional Tasks**
+
+1. Add card scaling feature.
+- Whenever a card is selected, the selected card and adjacent cards should be scaled up and moved up.
+![cardscale](CardScale.gif)
+
+2. Add card sorting feature.
+- Add card info to cards (number and type).
+- Add random card setting button and sorting button.
+- Sort cards in ascending order by number first, and then by type.
+- Sort order :
+(1 < 2 < 3 < 4 < 5 < 6 < 7 < 8 < 9 < 10 < J < Q < K)
+(Diamond < Clover < Heart < Spade)
+![cardsort](CardSort.gif)
+
+
+Need help? Email <u>qqas1211@naver.com</u>.
